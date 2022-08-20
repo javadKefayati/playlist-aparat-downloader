5b30496d4ae264141864c226f6e2071a7346eaa0
javad <javad936807@gmail.com>
2022-08-20 13:56:29 +0430

    init main.py for connect with user

---
+++ main.py
@@ -0,0 +1,30 @@
+import sys
+import list_downloader
+
+
+print("Hello")
+
+is_correct_url=False
+while is_correct_url==False:
+      print("Please enter directory you want to add videos :\n ***Note: your directory must be exist")
+      destination_file =input("Enter path : ")
+      if destination_file[-1]!="/":
+            print("***Your enter url is not correct must be end with /***\n")
+      else:
+            is_correct_url = True
+
+print("please choose between url playlist or id playlist???\n1-url\n2-id")
+choos_way =input("Enter a number : ") 
+
+url = ""
+
+if choos_way=="1":
+      url = input("Enter url :")
+if choos_way =="2":
+      id_playlist = input("Enter id : ")
+      url="https://www.aparat.com/playlist/"+id_playlist
+
+print("Please choose between quality:\n0 : 144p,\n1 : 240p,\n2 : 360p,\n3 : 480p,\n4 : 720p")
+quality_number = input("Enter a number :")
+
+list_downloader.Downloader(url,destination_file,int(quality_number))
